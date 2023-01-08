af6e7c96e71ffe03494f9d02ad8b53f68acdc0fa
pablo mereles <pablomereles62@gmail.com>
2023-01-08 20:51:05 -0300

    realice de nuevo de login

---
+++ main.js
@@ -0,0 +1,57 @@
+let inputs = document.getElementsByClassName('formulario-input');
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', function () {
+    if (this.value.length >= 1) {
+      this.nextElementSibling.classList.add('fijar')
+    } else {
+      this.nextElementsSibling.classList.remove('fijar')
+    }
+  })
+}
+
+function label () {
+  
+}
+//disable button
+let form = document.querySelector("#form")
+let btn = document.querySelector("#btn")
+
+function disableButton() {
+  let desabilitar = false;
+  if(form.email.value  === "") {
+    desabilitar = true;
+  }
+  if(form.password.value  === "") {
+    desabilitar = true;
+
+  }
+  if(desabilitar === true) {
+    btn.disabled = true;
+    this.classList.remove('form-btn')
+  } else{
+    btn.disabled = false;
+    this.classList.add('form-btn')
+   }
+  
+}
+
+form.addEventListener('keyup', disableButton) 
+
+ //password visibility
+let state = false; 
+function toggle () {
+  if(state) {
+    document.getElementById("password").setAttribute("type", "password");
+    document.getElementById('show').style.color = '#7a797e'
+    state = false;
+    
+  }
+  else {
+    document.getElementById("password").setAttribute("type", "text");
+    document.getElementById('show').style.color = '#33b18e'
+    state = true;
+
+  }
+}
+
+ 
